569803cf211143efe609285240df749cd5591872
DarushDev <darushfaqiri@gmail.com>
2019-03-08 15:05:26 +0800

    Change box image coordinates along with touch move event coordinates

--- app/src/main/java/com/example/exampleandroidflinganimation/MainActivity.java
+++ app/src/main/java/com/example/exampleandroidflinganimation/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.animation.DynamicAnimation;
 import android.support.animation.FlingAnimation;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -18,9 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     private int maxTranslationX;
     private int maxTranslationY;
+    private int extraHeight;
 
     ViewGroup mainLayout;
     ImageView ivBox;
+
+    FlingAnimation flingX;
+    FlingAnimation flingY;
+    private static final float FLING_MIN_TRANSLATION = 0;
+    private static final float FLING_FRICTION = 0.000001f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
         mainLayout = findViewById(R.id.layout_main);
         ivBox = findViewById(R.id.image_box);
+
+        flingX = new FlingAnimation(ivBox, DynamicAnimation.TRANSLATION_X);
+
+        flingY = new FlingAnimation(ivBox, DynamicAnimation.TRANSLATION_Y);
 
         final GestureDetector gestureDetector = new GestureDetector(this, mGestureListener);
 
@@ -44,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
             public void onGlobalLayout() {
                 maxTranslationX = mainLayout.getWidth() - ivBox.getWidth();
                 maxTranslationY = mainLayout.getHeight() - ivBox.getHeight();
+                Log.d(TAG, "onGlobalLayout: maxTranslationX:" + maxTranslationX + " maxTranslationY:" + maxTranslationY);
+                extraHeight = getPhoneHeight() - mainLayout.getHeight();
                 //As only wanted the first call back, so now remove the listener
                 mainLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
@@ -60,7 +73,27 @@ public class MainActivity extends AppCompatActivity {
         private static final float FRICTION = 1.1f;
 
         @Override
+        public boolean onScroll(MotionEvent downEvent, MotionEvent moveEvent, float distanceX, float distanceY) {
+            Log.d(TAG, "onScroll: @@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Log.d(TAG, "distanceX:" + distanceX + " X:" + moveEvent.getX() + " RawX:" + moveEvent.getRawX());
+            Log.d(TAG, "distanceY:" + distanceY + " Y:" + moveEvent.getY() + " RawY:" + moveEvent.getRawY());
+
+            ivBox.setTranslationX(moveEvent.getRawX() - (ivBox.getWidth() / 2));
+            ivBox.setTranslationY(moveEvent.getRawY() + extraHeight - (ivBox.getHeight() / 2));
+
+            return super.onScroll(downEvent, moveEvent, distanceX, distanceY);
+        }
+
+        @Override
         public boolean onDown(MotionEvent arg0) {
+            Log.d(TAG, "onDown: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            Log.d(TAG, "RawX: " + arg0.getRawX() + " X: " + arg0.getX());
+            Log.d(TAG, "RawY: " + arg0.getRawY() + " NewY: " + (arg0.getRawY()) + " Y: " + arg0.getY());
+
+            cancelFling();
+
+            ivBox.setTranslationX(arg0.getRawX() - (ivBox.getWidth() / 2));
+            ivBox.setTranslationY(arg0.getRawY() + extraHeight - (ivBox.getHeight() / 2));
             return true;
         }
 
@@ -72,29 +105,66 @@ public class MainActivity extends AppCompatActivity {
             float distanceInY = Math.abs(moveEvent.getRawY() - downEvent.getRawY());
 
             Log.d(TAG, "onFling: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            Log.d(TAG, "distanceInX : " + distanceInX + "\n" + "distanceInY : " + distanceInY);
+            Log.d(TAG, "distanceInX : " + distanceInX + "\t" + "distanceInY : " + distanceInY);
 
-            if (distanceInX > MIN_DISTANCE_MOVED) {
+            doFling(velocityX, velocityY);
+
+            /*if (distanceInX > MIN_DISTANCE_MOVED) {
                 //Fling Right/Left
                 FlingAnimation flingX = new FlingAnimation(ivBox, DynamicAnimation.TRANSLATION_X);
                 flingX.setStartVelocity(velocityX)
-                        .setMinValue(MIN_TRANSLATION) // minimum translationX property
+                        .setMinValue(FLING_MIN_TRANSLATION) // minimum translationX property
                         .setMaxValue(maxTranslationX)  // maximum translationX property
-                        .setFriction(FRICTION)
+                        .setFriction(FLING_FRICTION)
                         .start();
             } else if (distanceInY > MIN_DISTANCE_MOVED) {
                 //Fling Down/Up
                 FlingAnimation flingY = new FlingAnimation(ivBox, DynamicAnimation.TRANSLATION_Y);
                 flingY.setStartVelocity(velocityY)
-                        .setMinValue(MIN_TRANSLATION)  // minimum translationY property
+                        .setMinValue(FLING_MIN_TRANSLATION)  // minimum translationY property
                         .setMaxValue(maxTranslationY) // maximum translationY property
-                        .setFriction(FRICTION)
+                        .setFriction(FLING_FRICTION)
                         .start();
-            }
+            }*/
 
             return true;
         }
     };
+
+    private void cancelFling() {
+        if (flingX.isRunning()) {
+            flingX.cancel();
+        }
+
+        if (flingY.isRunning()) {
+            flingY.cancel();
+        }
+    }
+
+    private void doFling(float velocityX, float velocityY) {
+        Log.d(TAG, "doFling: velocityX: " + velocityX + " velocityY:" + velocityY);
+        cancelFling();
+
+        flingX.setStartVelocity(velocityX)
+                .setMinValue(FLING_MIN_TRANSLATION) // minimum translationX property
+                .setMaxValue(maxTranslationX)  // maximum translationX property
+                .setFriction(FLING_FRICTION)
+                .start();
+
+        flingY.setStartVelocity(velocityY)
+                .setMinValue(FLING_MIN_TRANSLATION)  // minimum translationY property
+                .setMaxValue(maxTranslationY) // maximum translationY property
+                .setFriction(FLING_FRICTION)
+                .start();
+    }
+
+    private int getPhoneHeight() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        //int width = displayMetrics.widthPixels;
+        return height;
+    }
 
     private void oldMethod() {
         final FlingAnimation flingAnimation = new FlingAnimation(ivBox, DynamicAnimation.TRANSLATION_Y);
@@ -133,4 +203,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
